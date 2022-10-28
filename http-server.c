011e6abe7f09d10049f8b4eb061b9eed0e257be5
KZNS <kzns@foxmail.com>
2022-10-28 09:24:48 +0800

    refactor: split listen and accept

--- http-server.c
+++ http-server.c
@@ -27,7 +27,7 @@
                    "\r\n"                       \
                    "hello world"
 
-void *http_server()
+int socket_listen(int port)
 {
     struct sockaddr_in srvaddr;
     int locfd;
@@ -38,14 +38,13 @@ void *http_server()
     locfd = socket(AF_INET, SOCK_STREAM, 0);
     if (locfd < 0)
     {
-        printf("create socket error!\n");
+        printf("create socket error! port: %d\n", port);
         exit(-1);
     }
-
-    printf("socket ready!\n");
+    printf("socket ready! locfd: %d\n", locfd);
 
     srvaddr.sin_family = AF_INET;
-    srvaddr.sin_port = htons(HTTP_PORT);
+    srvaddr.sin_port = htons(port);
     srvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     setsockopt(locfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
@@ -54,46 +53,58 @@ void *http_server()
     res = bind(locfd, (struct sockaddr *)&srvaddr, sizeof(srvaddr));
     if (res < 0)
     {
-        printf("bind error!\n");
+        printf("bind error! port: %d locfd: %d\n", port, locfd);
+        close(locfd);
+        exit(-1);
+    }
+    printf("bind ready! port: %d locfd: %d\n", port, locfd);
+
+    /*listen, 监听端口*/
+    listen(locfd, 10);
+    printf("waiting...\n");
+    return locfd;
+}
+
+int socket_accept(int locfd)
+{
+    struct sockaddr_in cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    int clifd;
+
+    clifd = accept(locfd, (struct sockaddr *)&cliaddr, &len);
+    if (clifd < 0)
+    {
+        printf("accept error! locfd: %d\n", locfd);
         close(locfd);
         exit(-1);
     }
 
-    printf("bind ready!\n");
+    /*输出客户机的信息*/
+    char *ip = inet_ntoa(cliaddr.sin_addr);
 
-    /*listen, 监听端口*/
-    listen(locfd, 10);
+    printf("ip %s connect to %d\n", ip, locfd);
 
-    printf("等待来自客户端的连接......\n");
+    /*输出客户机请求的信息*/
+    char buff[1024] = {0};
+    int size = read(clifd, buff, sizeof(buff));
+
+    printf("Request information: ");
+    printf("%s\n", buff);
+    printf("%d bytes\n", size);
+
+    return clifd;
+}
+
+void *http_server()
+{
+    int locfd = socket_listen(HTTP_PORT);
 
     while (1)
     {
-        struct sockaddr_in cliaddr;
-        socklen_t len = sizeof(cliaddr);
-        int clifd;
-
-        clifd = accept(locfd, (struct sockaddr *)&cliaddr, &len);
-        if (clifd < 0)
-        {
-            printf("accept error!\n");
-            close(locfd);
-            exit(-1);
-        }
-
-        /*输出客户机的信息*/
-        char *ip = inet_ntoa(cliaddr.sin_addr);
-
-        printf("客户机: %s连接到本服务器成功!\n", ip);
-
-        /*输出客户机请求的信息*/
-        char buff[1024] = {0};
-        int size = read(clifd, buff, sizeof(buff));
-
-        printf("Request information: ");
-        printf("%s\n", buff);
-        printf("%d bytes\n", size);
+        int clifd = socket_accept(locfd);
 
         write(clifd, MESSAGE301, strlen(MESSAGE301));
+
         close(clifd);
     }
 
@@ -102,71 +113,14 @@ void *http_server()
 
 void *https_server()
 {
-    struct sockaddr_in srvaddr;
-    int locfd;
-    int sockopt = 1;
-    int res;
-
-    /*创建一个套接字*/
-    locfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (locfd < 0)
-    {
-        printf("create socket error!\n");
-        exit(-1);
-    }
-
-    printf("socket ready!\n");
-
-    srvaddr.sin_family = AF_INET;
-    srvaddr.sin_port = htons(HTTPS_PORT);
-    srvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    setsockopt(locfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
-
-    /*bind, 将网络地址与端口绑定*/
-    res = bind(locfd, (struct sockaddr *)&srvaddr, sizeof(srvaddr));
-    if (res < 0)
-    {
-        printf("bind error!\n");
-        close(locfd);
-        exit(-1);
-    }
-
-    printf("bind ready!\n");
-
-    /*listen, 监听端口*/
-    listen(locfd, 10);
-
-    printf("等待来自客户端的连接......\n");
+    int locfd = socket_listen(HTTPS_PORT);
 
     while (1)
     {
-        struct sockaddr_in cliaddr;
-        socklen_t len = sizeof(cliaddr);
-        int clifd;
-
-        clifd = accept(locfd, (struct sockaddr *)&cliaddr, &len);
-        if (clifd < 0)
-        {
-            printf("accept error!\n");
-            close(locfd);
-            return NULL;
-        }
-
-        /*输出客户机的信息*/
-        char *ip = inet_ntoa(cliaddr.sin_addr);
-
-        printf("客户机: %s连接到本服务器成功!\n", ip);
-
-        /*输出客户机请求的信息*/
-        char buff[1024] = {0};
-        int size = read(clifd, buff, sizeof(buff));
-
-        printf("Request information: ");
-        printf("%s\n", buff);
-        printf("%d bytes\n", size);
+        int clifd = socket_accept(locfd);
 
         write(clifd, MESSAGE200, strlen(MESSAGE200));
+
         close(clifd);
     }
 
